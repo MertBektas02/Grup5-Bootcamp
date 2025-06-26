74f398c4b78e868058cd624384d0c9c8a6d82fc0
MuhtarBey <bektasmert2002@gmail.com>
2025-06-27 01:13:33 +0300

    basic food and water system. Player has basic needs now.

--- Assets/Mert Bektas/Scripts/Player Scripts/Player.cs
+++ Assets/Mert Bektas/Scripts/Player Scripts/Player.cs
@@ -6,12 +6,22 @@ public class Player : MonoBehaviour, IDataPersistence
     [Header("current stats")]
     public int currentHealth = 100;
     public int currentFood = 100;
-    public int currentWater=100;
+    public int currentWater = 100;
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
         Debug.Log("Player took damage. Current health: " + currentHealth);
+    }
+    void Start()
+    {
+        InvokeRepeating(nameof(ReduceNeeds), 0f, 5f); // 5 saniyede bir
+    }
+
+    void ReduceNeeds()
+    {
+        currentFood = Mathf.Max(currentFood - 1, 0);
+        currentWater = Mathf.Max(currentWater - 1, 0);
     }
     public void SaveData(ref GameData data)
     {
